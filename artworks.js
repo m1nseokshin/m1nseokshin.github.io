d5c7855bc539410674c48c8d1a5f3ced2bfcacd7
m1nseokshin <kevin5411@naver.com>
2023-05-12 13:25:15 +0900

    update

--- artworks.js
+++ artworks.js
@@ -3,12 +3,28 @@ lightbox.option({
     wrapAround: true,
     disableScrolling: true,
     fitImagesInViewport:true,
-    positionFromTop: 200
+    positionFromTop: 50
 })
 
-if (window.matchMedia("(min-width: 500px)").matches) { positionFromTop:0
-  } 
+if(matchMedia("screen and (max-width: 540px)").matches){
+  lightbox.option({    
+    resizeDuration: 200,
+    wrapAround: true,
+    disableScrolling: true,
+    fitImagesInViewport:true,
+    positionFromTop: 190
+  });
+}
+else if(matchMedia("screen and (max-width: 1024px)").matches){
+  lightbox.option({    
+    resizeDuration: 200,
+    wrapAround: true,
+    disableScrolling: true,
+    fitImagesInViewport:true,
+    positionFromTop: 200
+  });
+}
 
-  else {
-    positionFromTop:300
-  }
+window.onresize = function(){
+  document.location.reload();
+};
