007deb597d62026ef9c7f093cf23497b45cf8b49
m1nseokshin <kevin5411@naver.com>
2023-06-04 08:18:12 +0900

    final update

--- lightbox.js
+++ lightbox.js
@@ -8,19 +8,11 @@ lightbox.option({
 
 if(matchMedia("screen and (max-width: 540px)").matches){
   lightbox.option({    
-    resizeDuration: 200,
-    wrapAround: true,
-    disableScrolling: true,
-    fitImagesInViewport:true,
     positionFromTop: 190
   });
 }
 else if(matchMedia("screen and (max-width: 1024px)").matches){
   lightbox.option({    
-    resizeDuration: 200,
-    wrapAround: true,
-    disableScrolling: true,
-    fitImagesInViewport:true,
     positionFromTop: 200
   });
 }
