a4d2bd5b7fec16dfbf44878af7f121082e7a66ec
m1nseokshin <kevin5411@naver.com>
2023-07-26 00:34:26 +0900

    portfolio site redesign
    2학년 1학기 웹 디자인 기말 과제로 진행했던 프로젝트를 전부 갈아엎고 새로 디자인했습니다.

---
+++ changesvg.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function() {
+  var section2 = document.getElementById('section2');
+  var section3 = document.getElementById('section6');
+  var linksSVGs = document.querySelectorAll('.links img');
+
+  // #section2 요소가 뷰포트에 보이는지 확인
+  var section2Rect = section2.getBoundingClientRect();
+  var section3Rect = section3.getBoundingClientRect();
+  
+  var isVisibleSection2 = section2Rect.top >= 0 && section2Rect.bottom <= window.innerHeight;
+  var isVisibleSection3 = section3Rect.top >= 0 && section3Rect.bottom <= window.innerHeight;
+  
+  linksSVGs.forEach(function(svg) {
+    if (isVisibleSection2 || isVisibleSection3) {
+      svg.classList.add('white-svg');
+    } else {
+      svg.classList.remove('white-svg');
+    }
+  });
+});
---
+++ scrollscript.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+  const elm = document.querySelectorAll('#section2, #section3, #section4, #section5, #section6');
+  const elmCount = elm.length;
+  const duration = 250; // Set the duration of the scroll animation in milliseconds
+
+  function scrollToElement(element, duration) {
+    const startingY = window.pageYOffset;
+    const elementY = window.pageYOffset + element.getBoundingClientRect().top;
+    const targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY;
+    const diff = targetY - startingY;
+    let start;
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const time = timestamp - start;
+      const percent = Math.min(time / duration, 1);
+      window.scrollTo(0, startingY + diff * percent);
+      if (time < duration) {
+        window.requestAnimationFrame(step);
+      }
+    }
+
+    window.requestAnimationFrame(step);
+  }
+
+  elm.forEach(function(item, index) {
+    item.addEventListener('wheel', function(event) {
+      event.preventDefault();
+      let delta = 0;
+
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+        delta = event.wheelDelta / 120;
+        if (window.opera) delta = -delta;
+      } else if (event.detail) delta = -event.detail / 3;
+
+      let moveTop = window.scrollY;
+      const elmSelector = elm[index];
+
+      // wheel down: move to next section
+      if (delta < 0) {
+        if (elmSelector !== elmCount - 1) {
+          try {
+            scrollToElement(elmSelector.nextElementSibling, duration);
+          } catch (e) {}
+        }
+      }
+      // wheel up: move to previous section
+      else {
+        if (elmSelector !== 0) {
+          try {
+            scrollToElement(elmSelector.previousElementSibling, duration);
+          } catch (e) {}
+        }
+      }
+    });
+  });
+};
